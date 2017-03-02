dfdc79e0a0d06e09185832428ee3375e2c143f8d
seanphunt18 <seanphunt18@gmail.com>
2017-03-02 14:47:49 -0500

    full solution

--- app.js
+++ app.js
@@ -5,14 +5,31 @@ var state = {
 // State modification functions
 var addItem = function(state, item) {
     state.items.push(item);
+    // push the item to the items array
 };
 
-var toggleClass = function(item) {
-    item.toggleClass("shopping-item__checked");
+var toggleClass = function(state, itemText) {
+    for (var i=0; i<state.items.length; i++) {
+        if (state.items[i].text == itemText) {
+            // if the text of the current object in state matches the text of the item with the 'shopping-item' class falling under the closest li element to the button being clicked...(see line 66)
+            state.items[i].checked = !state.items[i].checked;
+            //set the value of the current object in state's 'checked' key to the opposite of its current value
+            break;
+            // stop looping
+        }
+    }
 }
 
-var removeItem = function(state, item) {
-    state.items.splice(item, 1);
+var removeItem = function(state, itemText) {
+    for (var i=0; i<state.items.length; i++) {
+        if (state.items[i].text == itemText) {
+            // same as above function
+            state.items.splice(i, 1);
+            // remove 1 object from splice, the current object in the loop
+            break;
+            // stop looping
+        }
+    }
 };
 
 // Render functions
@@ -37,29 +54,39 @@ $(function() {
 
     renderList(state, $('.shopping-list'));
 
-    $('#js-shopping-list-form').keydown(function(event){    
+    $('#js-shopping-list-form').keydown(function(event){
+    // if a key is pressed while engaged in the form with id 'js-shopping-list-form'...    
         if(event.keyCode==13){
+        // if the key pressed is the return key...
            $('#js-shopping-list-form').trigger('click');
+           // trigger a click event on the form
         }
     });
 
 
     $('#js-shopping-list-form').submit(function(event) {
+    // if the form with id 'js-shopping-list-form' is submitted...
         event.preventDefault();
+        // prevent the default element interaction from occurring
         addItem(state, {text: $('#shopping-list-entry').val(), checked: false});
+        // add an object to the items array, taking what is entered in the form's text area as the value for the text key, and false as the value for the checked key
         renderList(state, $('.shopping-list'));
+        // render the ul
     });
 
-    $('.shopping-item-toggle').click(function(event) {
+    $(document).on('click', '.shopping-item-toggle', function(event) {
+    // 
         event.preventDefault();
-        toggleClass($(event.target).closest("li"));
+        toggleClass(state, $(this).closest("li").find('.shopping-item').text());
+        // run the toggleClass function, taking the closest <li>'s descendant with class 'shopping-item''s text as itemText
         renderList(state, $('.shopping-list'));        
+        // render the ul
     });
 
-    $('.shopping-item-delete').click(function(event) {
+    $(document).on('click', '.shopping-item-delete', function(event) {
         event.preventDefault;
-        event.stopPropogation;
-        removeItem(state, $(event.target));
+        removeItem(state, $(this).closest("li").find('.shopping-item').text());
+        // run the removeItem function, taking the closest <li>'s descendant with class 'shopping-item''s text as itemTextremoveItem
         renderList(state, $('.shopping-list'));
     });
 
